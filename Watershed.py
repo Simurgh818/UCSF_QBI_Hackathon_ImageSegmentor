52644d86e9ecd3940d3803359355abdf66594838
naamirani <43768250+naamirani@users.noreply.github.com>
2019-10-26 13:55:30 -0700

    Update Watershed.py
    Enhance/brightness control - http://pippin.gimp.org/image_processing/chap_point.html

--- Watershed.py
+++ Watershed.py
@@ -2,8 +2,58 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
+# Brightness, contrast control - recommend B = 0, C = 60
+
+s = 128
+def apply_brightness_contrast(input_img, brightness = 0, contrast = 60):
+    input_img = cv2.resize(input_img, (s,s), 0, 0, cv2.INTER_AREA)
+    if brightness != 0:
+        if brightness > 0:
+            shadow = brightness
+            highlight = 255
+        else:
+            shadow = 0
+            highlight = 255 + brightness
+        alpha_b = (highlight - shadow)/255
+        gamma_b = shadow
+        buf = cv2.addWeighted(input_img, alpha_b, input_img, 0, gamma_b)
+    else:
+        buf = input_img.copy()
+    if contrast != 0:
+        f = 131*(contrast + 127)/(127*(131-contrast))
+        alpha_c = f
+        gamma_c = 127*(1-f)
+        buf = cv2.addWeighted(buf, alpha_c, buf, 0, gamma_c)
+    return buf
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+fcolor = (0,0,0)
+
+blist = [0, -127, 127,   0,  0, 64] # list of brightness values
+clist = [0,    0,   0, -64, 64, 64] # list of contrast values
+
+out = np.zeros((s*2, s*3, 3), dtype = np.uint8)
+
+for i, b in enumerate(blist):
+    c = clist[i]
+    print('b, c:  ', b,', ',c)
+    row = s*int(i/3)
+    col = s*(i%3)
+
+    print('row, col:   ', row, ', ', col)
+
+    out[row:row+s, col:col+s] = apply_brightness_contrast(img, b, c)
+    msg = 'b %d' % b
+    cv2.putText(out,msg,(col,row+s-22), font, .7, fcolor,1,cv2.LINE_AA)
+    msg = 'c %d' % c
+    cv2.putText(out,msg,(col,row+s-4), font, .7, fcolor,1,cv2.LINE_AA)
+    cv2.putText(out, 'OpenCV',(260,30), font, 1.0, fcolor,2,cv2.LINE_AA)
+    
+
 # Read image, make gray scale, threshold to binary
 img = cv.imread('mouse_brain-one_FOV.tif')
+# Enhance
+img = apply_brightness_contrast(img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
 
